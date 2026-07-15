9f7799e2301c6b3b361bd6ad4ad51192ee060cef
Jordan Marajh <marajhjordan@gmail.com>
2026-07-15 16:09:35 +0100

    Finished transform_poke_data.py script.

--- transform_poke_data.py
+++ transform_poke_data.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from pymongo.errors import PyMongoError
 
 MONGO_URI = "mongodb://localhost:27017/"
 DATABASE_NAME = "pokemon"
@@ -21,10 +22,10 @@ def transform_pokemon(collection):
             for entry in collection["types"]
         ],
 
-        "stats": [
-            {entry["stat"]["name"]: entry["base_stat"]}
+        "stats": {
+            entry["stat"]["name"]: entry["base_stat"] 
             for entry in collection["stats"]
-        ],
+        },
 
         "abilities": [
             {
@@ -43,28 +44,124 @@ def transform_pokemon(collection):
         ],
 
         # PokeAPI records weight in hectograms.
-        "weight_m": collection["weight"] / 10,
+        "weight_kg": collection["weight"] / 10,
 
         "sprite_url": collection["sprites"]["front_default"]
     }
+
+    return transformed_pokemon
+
+# Find the English description of a move.
+def get_english_effect(effect_entries):
+
+    for entry in effect_entries:
+        if entry["language"]["name"] == "en":
+            return entry["effect"]
+
+    return None
+
+# Simplify the information stored inside a move's meta field.
+def transform_meta(move_meta):
+
+    if move_meta is None:
+        return None
+
+    transformed_meta = {}
+
+    for key, value in move_meta.items():
+
+        if key in ["ailment", "category"]:
+            transformed_meta[key] = value["name"]
+
+        else:
+            transformed_meta[key] = value
+
+    return transformed_meta
     
 # Specify the exact attributes that we wish each Move to keep
 def transform_move(collection):
     
-    move_meta = collection.get("meta")
+    transformed_move = {}
 
-    # Specify the meta group if not empty, otherwise produce None
-    if move_meta is not None:
-        transformed_meta = {}
+    for key, value in collection.items():
 
-        for key, value in move_meta.items():
-            if key in ["ailment", "category"]:
-                transformed_meta[key] = value["name"]
-            else:
-                transformed_meta[key] = value
-    else:
-        trasnsformed_meta = None
+        if key in ["damage_class", "target", "type"]:
+            transformed_move[key] = value["name"]
+
+        elif key == "effect_entries":
+            transformed_move["effect"] = get_english_effect(value)
+
+        elif key == "learned_by_pokemon":
+            transformed_move[key] = [
+                pokemon["name"]
+                for pokemon in value
+            ]
+
+        elif key == "meta":
+            transformed_move[key] = transform_meta(value)
+
+        else:
+            transformed_move[key] = value
+
+    return transformed_move
 
 # Function to transform the data
 def transform_data():
-    pass
+
+    client = MongoClient(MONGO_URI)
+
+    try:
+        database = client[DATABASE_NAME]
+        pokemon_collection = database["pokemon"]
+        move_collection = database["move"]
+
+        ######################### Pokémon
+
+        print("Transforming Pokémon documents...")
+
+        pokemon_documents = pokemon_collection.find({})
+
+        pokemon_count = 0
+
+        for pokemon in pokemon_documents:
+            transformed_pokemon = transform_pokemon(pokemon)
+
+            pokemon_collection.replace_one(
+                {"_id": pokemon["_id"]},
+                transformed_pokemon
+            )
+
+            pokemon_count += 1
+
+        print(f"Transformed {pokemon_count} Pokémon documents.")
+
+        ######################### Moves
+
+        print("Transforming move documents...")
+
+        move_documents = move_collection.find({})
+
+        move_count = 0
+
+        for move in move_documents:
+            transformed_move = transform_move(move)
+
+            move_collection.replace_one(
+                {"_id": move["_id"]},
+                transformed_move
+            )
+
+            move_count += 1
+
+        print(f"Transformed {move_count} move documents.")
+
+        #########################
+
+    except PyMongoError as error:
+        print(f"MongoDB error: {error}")
+
+    except (KeyError, TypeError) as error:
+        print(f"Error transforming data: {error}")
+
+    finally:
+        client.close()
